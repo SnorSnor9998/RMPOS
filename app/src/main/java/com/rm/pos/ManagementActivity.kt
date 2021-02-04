e2e562cebb7b534bf2c6935448cc831506b3663e
SnorSnor9998 <56662976+SnorSnor9998@users.noreply.github.com>
2021-02-04 18:21:52 +0800

    chore: all side navigation on management

--- app/src/main/java/com/rm/pos/ManagementActivity.kt
+++ app/src/main/java/com/rm/pos/ManagementActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import com.rm.pos.MgnFragment.ProductFragment
 
 class ManagementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,4 +24,21 @@ class ManagementActivity : AppCompatActivity() {
 
         overridePendingTransition(0, 0)
     }
+
+    fun sidenav_click(view: View) {
+
+        when(view.id){
+            R.id.aty_mgn_sidenav_product -> replaceFragment(ProductFragment())
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val trans = supportFragmentManager.beginTransaction()
+        trans.addToBackStack(null)
+        trans.replace(R.id.aty_mgn_fragment_container,fragment)
+        trans.addToBackStack(null)
+        trans.commit()
+    }
+
 }
